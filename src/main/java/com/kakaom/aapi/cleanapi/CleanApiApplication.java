f63aef1dfd0588e20f5c608774218b4fed8c3c1e
khj874869 <khj874869@gmail.com>
2024-10-18 21:11:53 +0900

    2024-10-18
    카카오 정산하기 테스트

---
+++ src/main/java/com/kakaom/aapi/cleanapi/CleanApiApplication.java
@@ -0,0 +1,13 @@
+package com.kakaom.aapi.cleanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakaom/aapi/cleanapi/CleanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaom.aapi.cleanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
